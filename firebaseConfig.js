99bf12203652061533d5e5c54918d4b3d1a6837b
Dinesh9529 <121393084+Dinesh9529@users.noreply.github.com>
2025-06-16 16:53:48 +0530

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, RecaptchaVerifier } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB9Ay-fL_ai0-vSIeeYMKrWGB_Fk2huJYM",
+  authDomain: "sirohibazar-a1f79.firebaseapp.com",
+  projectId: "sirohibazar-a1f79",
+  storageBucket: "sirohibazar-a1f79.firebasestorage.app",
+  messagingSenderId: "943305647594",
+  appId: "1:943305647594:web:b0bf07a2583b5a2e763e93",
+  measurementId: "G-YX52VGN32V"
 };
 
 const app = initializeApp(firebaseConfig);
